c6a8fbe24beeecc80c25aee7dc16378c398b2618
Ricard Villalba <ricardax@gmail.com>
2022-08-27 14:19:02 +0200

    register and login working

--- src/views/Register.vue
+++ src/views/Register.vue
@@ -1,15 +1,20 @@
+
 <template>
   <div class="max-w-screen-sm mx-auto px-4 py-10">
-    <div v-if="errorMsg" class="mb-10 rounded-md bg-light-grey">
+    <!-- Error Handling -->
+    <div v-if="errorMsg" class="mb-10 p-4 rounded-md bg-light-grey shadow-lg">
       <p class="text-red-500">{{ errorMsg }}</p>
     </div>
 
-    <form class="p-8 flex flex-col bg-light-grey rounded-md shadow-lg">
+    <!-- Register -->
+    <form
+      @submit.prevent="register"
+      class="p-8 flex flex-col bg-light-grey rounded-md shadow-lg"
+    >
       <h1 class="text-3xl text-at-light-green mb-4">Register</h1>
-      <div class=" flex flex-col mb-2">
-        <label for="email" class="mb-1 text-sm text-at-light-green "
-          >Email</label
-        >
+
+      <div class="flex flex-col mb-2">
+        <label for="email" class="mb-1 text-sm text-at-light-green">Email</label>
         <input
           type="text"
           required
@@ -19,10 +24,8 @@
         />
       </div>
 
-      <div class=" flex flex-col mb-2">
-        <label for="password" class="mb-1 text-sm text-at-light-green "
-          >Password</label
-        >
+      <div class="flex flex-col mb-2">
+        <label for="password" class="mb-1 text-sm text-at-light-green">Password</label>
         <input
           type="password"
           required
@@ -32,8 +35,8 @@
         />
       </div>
 
-      <div class=" flex flex-col mb-2">
-        <label for="confirmPassword" class="mb-1 text-sm text-at-light-green "
+      <div class="flex flex-col mb-2">
+        <label for="confirmPassword" class="mb-1 text-sm text-at-light-green"
           >Confirm Password</label
         >
         <input
@@ -44,34 +47,61 @@
           v-model="confirmPassword"
         />
       </div>
+
       <button
         type="submit"
-        class="mt-6 py-2 px-6 self-start text-sm text-white bg-at-light-green duration-200 border-solid border-2 border-transparent hover:border-at-light-green hover:bg-white hover:text-at-light-green"
+        class="mt-6 py-2 px-6 rounded-sm self-start text-sm
+      text-white bg-at-light-green duration-200 border-solid
+      border-2 border-transparent hover:border-at-light-green hover:bg-white
+      hover:text-at-light-green"
       >
         Register
       </button>
 
-      <router-link
-        class="text-sm mt-6 text-center"
-        :to="{name:'Login'}"
-      >Already have an account <span class=" text-at-light-green">Login</span></router-link>
+      <router-link class="text-sm mt-6 text-center" :to="{ name: 'Login' }">
+        Already have an account? <span class="text-at-light-green">Login</span>
+      </router-link>
     </form>
   </div>
 </template>
 
 <script>
 import { ref } from "vue";
+import { supabase } from "../supabase/init";
+import { useRouter } from "vue-router";
 export default {
   name: "register",
   setup() {
     // Create data / vars
+    const router = useRouter();
     const email = ref(null);
     const password = ref(null);
     const confirmPassword = ref(null);
     const errorMsg = ref(null);
     // Register function
-
-    return { email, password, confirmPassword, errorMsg };
+    const register = async () => {
+      if (password.value === confirmPassword.value) {
+        try {
+          const { error } = await supabase.auth.signUp({
+            email: email.value,
+            password: password.value,
+          });
+          if (error) throw error;
+          router.push({ name: "Login" });
+        } catch (error) {
+          errorMsg.value = error.message;
+          setTimeout(() => {
+            errorMsg.value = null;
+          }, 5000);
+        }
+        return;
+      }
+      errorMsg.value = "Error: Passwords do not match";
+      setTimeout(() => {
+        errorMsg.value = null;
+      }, 5000);
+    };
+    return { email, password, confirmPassword, errorMsg, register };
   },
 };
 </script>
