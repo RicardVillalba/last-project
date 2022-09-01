324ff6690b322c95d1ed04c01cfc1eda6785a2b8
Ricard Villalba <ricardax@gmail.com>
2022-09-01 20:30:30 +0200

    sending tasks to supabase

--- src/views/Create.vue
+++ src/views/Create.vue
@@ -1,22 +1,97 @@
+
 <template>
-  <div></div>
+  <div class="max-w-screen-md mx-auto px-4 py-10">
+    <!-- Status Message -->
+    <div
+      v-if="statusMsg || errorMsg"
+      class="mb-10 p-4 bg-light-grey rounded-md shadow-lg"
+    >
+      <p class="text-at-light-green">
+        {{ statusMsg }}
+      </p>
+      <p class="text-red-500">{{ errorMsg }}</p>
+    </div>
+
+    <!-- Create -->
+    <div class="p-8 flex items-start bg-light-grey rounded-md shadow-lg">
+      <!-- Form -->
+      <form @submit.prevent="createtasks" class="flex flex-col gap-y-5 w-full">
+        <h1 class="text-2xl text-at-light-green">Record tasks</h1>
+
+        <!-- tasks Name -->
+        <div class="flex flex-col">
+          <label for="tasks-name" class="mb-1 text-sm text-at-light-green"
+            >tasks Name</label
+          >
+          <input
+            type="text"
+            required
+            class="p-2 text-gray-500 focus:outline-none"
+            id="tasks-name"
+            v-model="tasksName"
+          />
+        </div>
+
+
+        <button
+          type="submit"
+          class="mt-6 py-2 px-6 rounded-sm self-start text-sm
+      text-white bg-at-light-green duration-200 border-solid
+      border-2 border-transparent hover:border-at-light-green hover:bg-white
+      hover:text-at-light-green"
+        >
+          Add
+        </button>
+      </form>
+    </div>
+  </div>
 </template>
 
 <script>
+import { ref } from "vue";
+// import { uid } from "uid";
+import { supabase } from "../supabase/init";
 export default {
   name: "create",
   setup() {
     // Create data
-
+    const tasksName = ref("");
+    const statusMsg = ref(null);
+    const errorMsg = ref(null);
     // Add exercise
+ 
 
-    // Delete exercise
-
-    // Listens for chaging of workout type input
-
-    // Create workout
-
-    return {};
+  
+    // Create tasks
+    const createtasks = async () => {
+      try {
+        const { error } = await supabase.from("tasks").insert([
+          {
+            name: name.value,
+         
+          },
+        ]);
+        if (error) throw error;
+        statusMsg.value = "Succes: tasks Created!";
+        tasksName.value = null;
+        
+        
+        setTimeout(() => {
+          statusMsg.value = false;
+        }, 5000);
+      } catch (error) {
+        errorMsg.value = `Error: ${error.message}`;
+        setTimeout(() => {
+          errorMsg.value = false;
+        }, 5000);
+      }
+    };
+    return {
+      tasksName,
+      statusMsg,
+      errorMsg,
+      createtasks,
+    };
   },
 };
 </script>
