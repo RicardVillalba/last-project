c10c7a8db16a878f18adc28c6cc8cb1bead41021
Hiro <ricardvillalbaburrull@MacBook-Pro-de-Hiro.local>
2022-09-15 19:46:39 +0200

    register text color modified

--- src/App.vue
+++ src/App.vue
@@ -34,7 +34,6 @@ export default {
 };
 </script>
 
-
 <style lang="scss">
 @import url('https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500;600&display=swap');
 </style>
--- src/views/Register.vue
+++ src/views/Register.vue
@@ -11,10 +11,10 @@
       @submit.prevent="register"
       class="p-8 flex flex-col bg-light-grey rounded-md shadow-lg"
     >
-      <h1 class="text-3xl text-at-light-green mb-4">Register</h1>
+      <h1 class="text-3xl text-black mb-4">Register</h1>
 
       <div class="flex flex-col mb-2">
-        <label for="email" class="mb-1 text-sm text-at-light-green">Email</label>
+        <label for="email" class="mb-1 text-sm text-black">Email</label>
         <input
           type="text"
           required
@@ -25,7 +25,7 @@
       </div>
 
       <div class="flex flex-col mb-2">
-        <label for="password" class="mb-1 text-sm text-at-light-green">Password</label>
+        <label for="password" class="mb-1 text-sm text-black">Password</label>
         <input
           type="password"
           required
@@ -36,7 +36,7 @@
       </div>
 
       <div class="flex flex-col mb-2">
-        <label for="confirmPassword" class="mb-1 text-sm text-at-light-green"
+        <label for="confirmPassword" class="mb-1 text-sm text-black"
           >Confirm Password</label
         >
         <input
@@ -53,13 +53,13 @@
         class="mt-6 py-2 px-6 rounded-sm self-start text-sm
       text-white bg-at-light-green duration-200 border-solid
       border-2 border-transparent hover:border-at-light-green hover:bg-white
-      hover:text-at-light-green"
+      hover:text-black"
       >
         Register
       </button>
 
       <router-link class="text-sm mt-6 text-center" :to="{ name: 'Login' }">
-        Already have an account? <span class="text-at-light-green">Login</span>
+        Already have an account? <span class="text-black">Login</span>
       </router-link>
     </form>
   </div>
