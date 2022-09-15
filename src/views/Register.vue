c543371e4387fa5eee5a0d9734bef9182ae7e772
RicardVillalba <ricardsax@gmail.com>
2022-09-15 20:22:38 +0200

    register btn css fixed

--- src/views/Register.vue
+++ src/views/Register.vue
@@ -46,17 +46,15 @@
           v-model="confirmPassword"
         />
       </div>
-
       <button
         type="submit"
         class="mt-6 py-2 px-6 rounded-sm self-start text-sm
-      text-white bg-at-light-green duration-200 border-solid
-      border-2 border-transparent hover:border-at-light-green hover:bg-white
+      text-white bg-black duration-200 border-solid
+      border-2 border-transparent hover:border-black hover:bg-white
       hover:text-black"
       >
         Register
       </button>
-
       <router-link class="text-sm mt-6 text-center" :to="{ name: 'Login' }">
         Already have an account? <span class="text-black">Login</span>
       </router-link>
